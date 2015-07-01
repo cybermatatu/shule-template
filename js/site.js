127cd1ed79647c2fced5ff0fc7a91d18dcc7f39e
Fredrick Njenga <cybermatatu@gmail.com>
2015-07-01 14:52:12 +0300

    Re-Added Site Footer - Fixed Issue#1

--- js/site.js
+++ js/site.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 
   	$(".owl-testimonies").owlCarousel({
-  		items: 4,
+  		items: 3,
   		navigation : false,
   		navigationText : ['<span class="fa fa-chevron-left"></span>','<span class="fa fa-chevron-right"></span>'],
   	});
