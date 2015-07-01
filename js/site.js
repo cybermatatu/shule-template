b1e2e45cd1f95dc332072f6661aa70df75fa43c1
Fredrick Njenga <cybermatatu@gmail.com>
2015-07-01 15:10:58 +0300

    Fixed Testimonials Margin Issue

--- js/site.js
+++ js/site.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 
   	$(".owl-testimonies").owlCarousel({
-  		items: 3,
+  		items: 4,
   		navigation : false,
   		navigationText : ['<span class="fa fa-chevron-left"></span>','<span class="fa fa-chevron-right"></span>'],
   	});
