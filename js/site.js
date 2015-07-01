4b14480fef0919517fa82ab903c5117b1908e4e4
Fredrick Njenga <cybermatatu@gmail.com>
2015-07-01 10:16:33 +0300

    Added Testimonials

---
+++ js/site.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+
+  	$("#owl-testimonies").owlCarousel({
+  		items: 4,
+  		navigation : false,
+  		navigationText : ['<span class="fa fa-chevron-left"></span>','<span class="fa fa-chevron-right"></span>'],
+  	});
+});
